27e6a5cac939f1221f424a715d21bf40db951048
Paul Mason <63802301+PaulMasonDev@users.noreply.github.com>
2023-02-21 05:46:09 -0800

    Update constants.ts with live backend

--- src/clientLibrary/constants.ts
+++ src/clientLibrary/constants.ts
@@ -1,1 +1,2 @@
-export const DATA_API_URL = "http://localhost:5000";
+// export const DATA_API_URL = "http://localhost:5000";
+export const DATA_API_URL = "https://andromeda-space-game.herokuapp.com";
